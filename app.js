8e8ede1b7b8f51999dc612c24bbd25627575358a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-06-16 23:51:00 -0700

    building logic

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+var items =[];
+
+app.get("/", function(req, res){
+var today = new Date();
+var options= {
+weekday: "long",
+day: "numeric",
+month: "long"
+};
+
+var day = today.toLocaleString("en-US", options);
+
+res.render("list", {kindOfDay:day, newListItems:items});
+
+});
+
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+
+items.push(item);
+res.redirect("/")
+
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on Port 3000.");
+})
