6c7aa3aa33ddd2bf66f4034fec478158d59ede0c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-06-29 15:45:12 -0700

    read data from db

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getDate } = require('./date');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-const items =[];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/taskmanagerDB", {useNewUrlParser:true, useUnifiedTopology:true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model(
+  "item", itemsSchema
+);
+
+const item1 = new Item({
+  name: "make food"
+});
+
+const item2 = new Item({
+  name: "go to work"
+});
+
+const item3 = new Item({
+  name: "bathe baby"
+});
+
+
+const arr = [item1, item2, item3];
 
 app.get("/", function(req, res){
+  Item.find({}, function(err, foundItems){
 
-  const day = date.getDate();
-  res.render("list", {listTitle:day, newListItems:items});
+    if(foundItems.length===0){
+        Item.insertMany(arr, function(err){
+              if(err){
+              console.log(err);
+                }else{
+            console.log("Successfully saved to DB");
+          }
+
+        });
+res.redirect("/")
+      } else{
+        res.render("list", {listTitle:"Today", newListItems:foundItems});  
+        }    
+  })
 
 });
 
